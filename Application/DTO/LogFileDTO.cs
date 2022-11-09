3dbe78ddc5d9499c9a975b410eec107a6b90c671
dasnie97 <damiansnieda97@gmail.com>
2022-11-09 19:52:57 +0100

    Changed nullability for LogFileDTO

--- Application/DTO/LogFileDTO.cs
+++ Application/DTO/LogFileDTO.cs
@@ -3,9 +3,9 @@
     public class LogFileDTO
     {
         public int Id { get; set; }
-        public string Workstation { get; set; } = null!;
-        public string SerialNumber { get; set; } = null!;
-        public string Status { get; set; } = null!;
+        public string? Workstation { get; set; } = null!;
+        public string? SerialNumber { get; set; } = null!;
+        public string? Status { get; set; } = null!;
         public string? FixtureSocket { get; set; }
         public string? Failure { get; set; }
         public string? Operator { get; set; }
