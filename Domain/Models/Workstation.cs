9159c10cd6a2c76c503d8e7e84dc56b74707f674
dasnie97 <damiansnieda97@gmail.com>
2023-03-25 13:24:01 +0100

    Removed inheritance from WorkstationBase as it doesnt exist anymore

--- Domain/Models/Workstation.cs
+++ Domain/Models/Workstation.cs
@@ -1,14 +1,15 @@
-﻿using ProductTest.Common;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Models;
 
 [Table("Workstations")]
-public class Workstation : WorkstationBase
+public class Workstation
 {
     [Key]
     public int Id { get; private set; }
+    public string Name { get; set; }
+    public string OperatorName { get; set; }
     public string? Customer { get; set; }
     public string? PositionX { get; set; }
     public string? PositionY { get; set; }
@@ -16,8 +17,9 @@ public class Workstation : WorkstationBase
     public DateTime RecordCreated { get; set; }
     public DateTime RecordUpdated { get; set; }
 
-    public Workstation(string name) : base(name)
+    public Workstation(string name = "", string operatorName = "")
     {
-
+        Name = name;
+        OperatorName = operatorName;
     }
 }
