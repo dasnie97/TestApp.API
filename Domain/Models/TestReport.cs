01af2399d000959da72da6a9fbc3c158d1b81c8b
dasnie97 <damiansnieda97@gmail.com>
2023-04-01 19:00:36 +0200

    API is working with TestReport.Workstation of string type

--- Domain/Models/TestReport.cs
+++ Domain/Models/TestReport.cs
@@ -7,6 +7,7 @@ public class TestReport : ProductTest.Models.TestReport
     public int Id { get; private set; }
     public bool IsFirstPass { get; set; }
     public bool IsFalseCall { get; set; }
+    //TODO: Make this of WorkstationBase type
     public new string Workstation { get; set; }
     public string? ProcessStep { get; set; }
     public DateTime RecordCreated { get; set; }
