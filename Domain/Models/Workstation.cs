c62334d4bc0375ce638dd182db5a0814a69c004c
dasnie97 <damiansnieda97@gmail.com>
2023-04-19 18:22:52 +0200

    Removed ID, Customer and Positions properties

--- Domain/Models/Workstation.cs
+++ Domain/Models/Workstation.cs
@@ -2,13 +2,9 @@
 
 public class Workstation
 {
-    public int Id { get; set; }
     public string Name { get; set; }
     public string? OperatorName { get; set; }
     public string ProcessStep { get; set; }
-    public string? Customer { get; set; }
-    public string? PositionX { get; set; }
-    public string? PositionY { get; set; }
     public string? State { get; set; }
     public DateTime RecordCreated { get; set; }
     public DateTime RecordUpdated { get; set; }
