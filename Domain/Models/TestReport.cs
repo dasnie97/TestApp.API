b8fdb238f9c5884f29197b10ed6650faa125bc8e
dasnie97 <damiansnieda97@gmail.com>
2023-04-19 17:31:11 +0200

    Property order changed

--- Domain/Models/TestReport.cs
+++ Domain/Models/TestReport.cs
@@ -5,13 +5,13 @@ public class TestReport
     public int Id { get; set; }
     public Workstation Workstation { get; set; }
     public string WorkstationName { get; set; }
-    public bool IsFirstPass { get; set; }
-    public bool IsFalseCall { get; set; }
     public string SerialNumber { get; set; }
     public TestStatus Status { get; set; }
-    public string Failure { get; set; }
-    public string? FixtureSocket { get; set; }
     public DateTime TestDateTimeStarted { get; set; }
     public TimeSpan? TestingTime { get; set; }
+    public string? FixtureSocket { get; set; }
+    public string Failure { get; set; }
+    public bool IsFirstPass { get; set; }
+    public bool IsFalseCall { get; set; }
     public DateTime RecordCreated { get; set; }
 }
