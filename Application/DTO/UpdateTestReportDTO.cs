fff15b7c07a6ca9e7ea48f3ae759acd9012c8c36
dasnie97 <damiansnieda97@gmail.com>
2023-05-23 18:10:04 +0200

    Removed unused properties

--- Application/DTO/UpdateTestReportDTO.cs
+++ Application/DTO/UpdateTestReportDTO.cs
@@ -3,13 +3,11 @@
 public class UpdateTestReportDTO
 {
     public int Id { get; set; }
-    public string Workstation { get; set; } = null!;
-    public string SerialNumber { get; set; } = null!;
-    public string Status { get; set; } = null!;
-    public string? FixtureSocket { get; set; }
-    public string? Failure { get; set; }
-    public string? Operator { get; set; }
-    public string? TestProgramFilePath { get; set; }
+    public string Workstation { get; set; }
+    public string SerialNumber { get; set; }
+    public string Status { get; set; }
     public DateTime TestDateTimeStarted { get; set; }
-    public TimeSpan? TestingTime { get; set; }
+    public TimeSpan TestingTime { get; set; }
+    public string FixtureSocket { get; set; }
+    public string Failure { get; set; }
 }
