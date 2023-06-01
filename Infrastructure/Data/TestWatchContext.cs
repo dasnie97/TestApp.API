a8364880cb43026e6780972716ed44149b551b8e
dasnie97 <damiansnieda97@gmail.com>
2023-06-01 18:44:03 +0200

    Added MySql.EntityFrameworkCore. Moved DBContext configuration from Program.cs. Database provider is chosen basing on value set in appsettings.json.

--- Infrastructure/Data/TestWatchContext.cs
+++ Infrastructure/Data/TestWatchContext.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.Data;
 
@@ -7,9 +8,29 @@ public class TestWatchContext : DbContext
 {
     public DbSet<TestReport> TestReports { get; set; }
     public DbSet<Workstation> Workstations { get; set; }
-    public TestWatchContext(DbContextOptions options) : base(options)
+
+    private IConfiguration _configuration;
+
+    public TestWatchContext(DbContextOptions options, IConfiguration configuration) : base(options)
     {
-        
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var dbType = _configuration["DBType"];
+        if (dbType == "SQLServer")
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("SQLServer"));
+        }
+        else if (dbType == "MySQL")
+        {
+            optionsBuilder.UseMySQL(_configuration.GetConnectionString("MySQL"));
+        }
+        else
+        {
+            throw new Exception("DB type not chosen! Choose either 'SQLServer' or 'MySQL' in appsettings.json");
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
