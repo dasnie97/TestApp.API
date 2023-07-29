dfde45e16af98d96399882f3d442a9922632cf75
dasnie97 <damiansnieda97@gmail.com>
2023-07-29 10:46:24 +0200

    Helper for store time intervals (DateTime pairs)

---
+++ Infrastructure/Helpers/TimeInterval.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Helpers;
+
+public class TimeInterval
+{
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+
+    public TimeInterval(DateTime start, DateTime end)
+    {
+        Start = start;
+        End = end;
+    }
+}
